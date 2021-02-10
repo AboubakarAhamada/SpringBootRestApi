99605d74bee916a4381e3a3c9d0357d63af11606
AboubakarAhamada <aboumarshal@hotmail.fr>
2021-02-10 15:21:23 +0100

    Test with Jenkins

--- src/main/java/com/aboubakar/RestApiApplication.java
+++ src/main/java/com/aboubakar/RestApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("========= YES IT WORKS =========");
 		SpringApplication.run(RestApiApplication.class, args);
 		
 		
